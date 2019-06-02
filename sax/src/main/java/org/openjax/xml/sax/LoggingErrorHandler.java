59749e72048f9ef17c426349c12b1307702f632a
Seva Safris <seva@safris.org>
2019-06-02 22:34:59 +0200

    Add configurable log levels

--- sax/src/main/java/org/openjax/xml/sax/LoggingErrorHandler.java
+++ sax/src/main/java/org/openjax/xml/sax/LoggingErrorHandler.java
@@ -18,6 +18,7 @@ package org.openjax.xml.sax;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -25,24 +26,56 @@ import org.xml.sax.SAXParseException;
 public class LoggingErrorHandler implements ErrorHandler {
   private static final Logger logger = LoggerFactory.getLogger(LoggingErrorHandler.class);
 
+  private static void log(final String message, final SAXParseException e, final Level level) {
+    if (level == null)
+      return;
+
+    if (level == Level.TRACE)
+      logger.trace(message, e);
+    else if (level == Level.DEBUG)
+      logger.debug(message, e);
+    else if (level == Level.INFO)
+      logger.info(message, e);
+    else if (level == Level.WARN)
+      logger.warn(message, e);
+    else if (level == Level.ERROR)
+      logger.error(message, e);
+    else
+      throw new UnsupportedOperationException("Unsupported log level: " + level);
+  }
+
+  private final Level warn;
+  private final Level error;
+  private final Level fatalError;
+
+  public LoggingErrorHandler(final Level warn, final Level error, final Level fatalError) {
+    this.warn = warn;
+    this.error = error;
+    this.fatalError = fatalError;
+  }
+
+  public LoggingErrorHandler() {
+    this(Level.WARN, Level.ERROR, Level.ERROR);
+  }
+
   @Override
   public void warning(final SAXParseException exception) throws SAXException {
     final String message = exception.getMessage() + " (" + exception.getLineNumber() + "," + exception.getColumnNumber() + ")";
     if (exception.getMessage() != null && exception.getMessage().startsWith("schema_reference.4")) {
-      logger.error(message);
+      log(message, null, error);
       throw exception;
     }
 
-    logger.warn(message);
+    log(message, null, warn);
   }
 
   @Override
   public void error(final SAXParseException exception) throws SAXException {
-    logger.error(exception.getMessage() + " (" + exception.getLineNumber() + "," + exception.getColumnNumber() + ")");
+    log(exception.getMessage() + " (" + exception.getLineNumber() + "," + exception.getColumnNumber() + ")", null, error);
   }
 
   @Override
   public void fatalError(final SAXParseException exception) throws SAXException {
-    logger.error(exception.getMessage() + " (" + exception.getLineNumber() + "," + exception.getColumnNumber() + ")");
+    log(exception.getMessage() + " (" + exception.getLineNumber() + "," + exception.getColumnNumber() + ")", null, fatalError);
   }
 }
