802987f41a65dac173fe87b60088946bdb24b8f6
Seva Safris <seva@safris.org>
2019-06-02 22:35:13 +0200

    Implement SilentErrorHandler

---
+++ sax/src/main/java/org/openjax/xml/sax/SilentErrorHandler.java
@@ -0,0 +1,38 @@
+/* Copyright (c) 2019 OpenJAX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.openjax.xml.sax;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ * An {@link ErrorHandler} implementation that produces no output.
+ */
+public class SilentErrorHandler implements ErrorHandler {
+  @Override
+  public void warning(final SAXParseException exception) throws SAXException {
+  }
+
+  @Override
+  public void error(final SAXParseException exception) throws SAXException {
+  }
+
+  @Override
+  public void fatalError(final SAXParseException exception) throws SAXException {
+  }
+}
