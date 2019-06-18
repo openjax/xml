9e6e235dbfd201f633559e29977f2b0ebed7865b
Seva Safris <seva@safris.org>
2019-06-18 21:37:15 +0200

    Fix concurrency issue

--- sax/src/main/java/org/openjax/xml/sax/Parsers.java
+++ sax/src/main/java/org/openjax/xml/sax/Parsers.java
@@ -1,3 +1,19 @@
+/* Copyright (c) 2019 OpenJAX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
 package org.openjax.xml.sax;
 
 import javax.xml.parsers.FactoryConfigurationError;
@@ -14,17 +30,6 @@ import org.xml.sax.SAXException;
  */
 public final class Parsers {
   private static final Logger logger = LoggerFactory.getLogger(Parsers.class);
-  private static SAXParserFactory factory;
-
-  static {
-    try {
-      factory = SAXParserFactory.newInstance("org.apache.xerces.jaxp.SAXParserFactoryImpl", null);
-    }
-    catch (final FactoryConfigurationError e) {
-      factory = SAXParserFactory.newInstance();
-      logger.warn("Unable to create SAXParserFactory of type org.apache.xerces.jaxp.SAXParserFactoryImpl. Factory of " + factory.getClass().getName() + " created instead.", e);
-    }
-  }
 
   /**
    * Creates a new {@link SAXParser} instance that supports:
@@ -43,6 +48,15 @@ public final class Parsers {
    * @throws SAXException If a SAX exception has occurred.
    */
   public static SAXParser newParser(final boolean validating) throws SAXException {
+    SAXParserFactory factory;
+    try {
+      factory = SAXParserFactory.newInstance("org.apache.xerces.jaxp.SAXParserFactoryImpl", null);
+    }
+    catch (final FactoryConfigurationError e) {
+      factory = SAXParserFactory.newInstance();
+      logger.warn("Unable to create SAXParserFactory of type org.apache.xerces.jaxp.SAXParserFactoryImpl. Factory of " + factory.getClass().getName() + " created instead.", e);
+    }
+
     factory.setNamespaceAware(true);
     factory.setValidating(true);
     try {
