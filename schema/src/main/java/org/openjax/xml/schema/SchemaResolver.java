08ccbd3abc04ed7a12b973b425061cc6db307b3a
Seva Safris <seva@safris.org>
2021-08-20 15:31:56 +0700

    Rewrite SchemaResolver

--- schema/src/main/java/org/openjax/xml/schema/SchemaResolver.java
+++ schema/src/main/java/org/openjax/xml/schema/SchemaResolver.java
@@ -20,56 +20,72 @@ import java.net.URL;
 
 import javax.xml.XMLConstants;
 
+/**
+ * Utility class that resolves the schema location of schemas identified by the
+ * following {@code publicId} and {@code systemId}:
+ *
+ * <pre>
+ * http://www.w3.org/2001/XMLSchema
+ * http://www.w3.org/XML/1998/namespace http://www.w3.org/2001/xml.xsd
+ * </pre>
+ */
 public final class SchemaResolver {
-  private static final Entry[] entries = new Entry[2];
+  private static final Schema[] schemas = new Schema[] {
+    new Schema(XMLConstants.W3C_XML_SCHEMA_NS_URI, null, "xmlschema/XMLSchema.xsd"),
+    new Schema(XMLConstants.XML_NS_URI, "http://www.w3.org/2001/xml.xsd", "xmlschema/xml.xsd")
+  };
 
-  private static class Entry {
+  private static class Schema {
     private final String publicId;
     private final String systemId;
     private final String resourcePath;
     private URL url;
 
-    private Entry(final String publicId, final String systemId, final String resourcePath) {
+    private Schema(final String publicId, final String systemId, final String resourcePath) {
       this.publicId = publicId;
       this.systemId = systemId;
       this.resourcePath = resourcePath;
     }
-  }
 
-  private static Entry getEntry(final String publicId, final String systemId) {
-    if (publicId == null)
-      return null;
-
-    for (int i = 0; i < entries.length; ++i) {
-      final Entry entry = entries[i];
-      if (publicId.equals(entry.publicId) && (systemId == null || systemId.equals(entry.systemId)))
-        return entry;
+    @Override
+    public String toString() {
+      return "{" + publicId + "}" + systemId;
     }
-
-    return null;
   }
 
-  static {
-    entries[0] = new Entry(XMLConstants.W3C_XML_SCHEMA_NS_URI, null, "xmlschema/XMLSchema.xsd");
-    entries[1] = new Entry(XMLConstants.XML_NS_URI, "http://www.w3.org/2001/xml.xsd", "xmlschema/xml.xsd");
-  }
-
+  /**
+   * Returns the schema location {@link URL} for the specified {@code publicId}
+   * and {@code systemId}, or {@code null} if the provided {@code publicId} and
+   * {@code systemId} do not match one of:
+   *
+   * <pre>
+   * http://www.w3.org/2001/XMLSchema
+   * http://www.w3.org/XML/1998/namespace http://www.w3.org/2001/xml.xsd
+   * </pre>
+   *
+   * @param publicId The Public ID.
+   * @param systemId The System ID.
+   * @return The schema location {@link URL} for the specified {@code publicId}
+   *         and {@code systemId}.
+   */
   public static URL resolve(final String publicId, final String systemId) {
     if (publicId == null)
       return null;
 
-    final Entry entry = getEntry(publicId, systemId);
-    if (entry == null)
-      return null;
+    for (final Schema schema : schemas) {
+      if (publicId.equals(schema.publicId) && (systemId == null || systemId.equals(schema.systemId))) {
+        if (schema.url != null)
+          return schema.url;
 
-    if (entry.url != null)
-      return entry.url;
+        final URL url = ClassLoader.getSystemClassLoader().getResource(schema.resourcePath);
+        if (url == null)
+          throw new IllegalStateException("Unable to find location of schema: " + schema);
 
-    final URL url = SchemaResolver.class.getClassLoader().getResource(entry.resourcePath);
-    if (url == null)
-      throw new IllegalStateException();
+        return schema.url = url;
+      }
+    }
 
-    return entry.url = url;
+    return null;
   }
 
   private SchemaResolver() {
